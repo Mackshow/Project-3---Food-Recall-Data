afd12a78fdabed3acca60f292d5594c0c6139af7
larabrry <laraberry10@gmail.com>
2024-01-15 22:40:56 -0500

    database and flask setup

--- flask/app.py
+++ flask/app.py
@@ -8,36 +8,38 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, render_template, request, jsonify, redirect
 
-# create instance of Flask app
-app = Flask(__name__)
 
-# setup database
-engine = create_engine("sqlite:///db/foodReactions.sqlite", echo=False)
+##########################################
+# Database Setup
+##########################################
 
+engine = create_engine("sqlite:///db/foodReactions0.sqlite", echo=False)
 
-Base = automap_base()
-Base.prepare(engine, reflect=True)
+# Base definition, reflect an existing DB into a new model
+base = automap_base()
 
+# Reflect the DB Tables
+base.prepare(engine, reflect=True)
+
+#Save references to each table
+reactions= base.classes.reactions
+prooducts= base.classes.products
+
+#Create our session from Python to the DB
 session = Session(engine)
 
+
+################################################
+# Flask Setup
+##################################################
+
+app = Flask(__name__)
 
 # create route that renders index.html template
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        age = request.form.get('age')
-        return f'Thank you, {name}! You are {age} years old.'
-
-@app.route('/greet', methods=['GET'])
-def greet():
-    name = request.args.get('name', 'Guest')
-    return f'Hello, {name}!'
 
 
 if __name__ == '__main__':
