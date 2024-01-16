47d4eb0f27b253422029eefa5ed0b90716db803a
larabrry <laraberry10@gmail.com>
2024-01-16 05:38:49 -0500

    add diarrheaBars JS file

---
+++ flask/static/js/diarrheaBars.js
@@ -0,0 +1,75 @@
+Highcharts.chart('container', {
+    colors: ['#FFD700', '#C0C0C0', '#CD7F32'],
+    chart: {
+        type: 'column',
+        inverted: true,
+        polar: true
+    },
+    title: {
+        text: 'Diarrhea-causing Food (TOP 5)',
+        align: 'left'
+    },
+    
+    tooltip: {
+        outside: true
+    },
+    pane: {
+        size: '85%',
+        innerSize: '20%',
+        endAngle: 270
+    },
+    xAxis: {
+        tickInterval: 1,
+        labels: {
+            align: 'right',
+            useHTML: true,
+            allowOverlap: true,
+            step: 1,
+            y: 3,
+            style: {
+                fontSize: '13px'
+            }
+        },
+        lineWidth: 0,
+        gridLineWidth: 0,
+        categories: [
+            'Norway <span class="f16"><span id="flag" class="flag no">' +
+            '</span></span>',
+            'United States <span class="f16"><span id="flag" class="flag us">' +
+            '</span></span>',
+            'Germany <span class="f16"><span id="flag" class="flag de">' +
+            '</span></span>',
+            'Austria <span class="f16"><span id="flag" class="flag at">' +
+            '</span></span>',
+            'Canada <span class="f16"><span id="flag" class="flag ca">' +
+            '</span></span>'
+        ]
+    },
+    yAxis: {
+        lineWidth: 0,
+        tickInterval: 25,
+        reversedStacks: false,
+        endOnTick: true,
+        showLastLabel: true,
+        gridLineWidth: 0
+    },
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            borderWidth: 0,
+            pointPadding: 0,
+            groupPadding: 0.15,
+            borderRadius: '50%'
+        }
+    },
+    series: [{
+        name: 'Gold medals',
+        data: [148, 113, 104, 71, 77]
+    }, {
+        name: 'Silver medals',
+        data: [113, 122, 98, 88, 72]
+    }, {
+        name: 'Bronze medals',
+        data: [124, 95, 65, 91, 76]
+    }]
+});
