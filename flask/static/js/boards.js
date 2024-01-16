100c0c0dba357fc11199d98120ef053181a5fa21
larabrry <laraberry10@gmail.com>
2024-01-16 06:51:43 -0500

    add KPI file

---
+++ flask/static/js/boards.js
@@ -0,0 +1,17 @@
+components: [{
+    cell: 'dashboard-kpi-1',
+    type: 'KPI',
+    title: 'Food Reactions',
+    subtitle: 'Total Number of Food Reactions for This Year',
+    linkedValueTo: {
+        enabled: false
+    }
+}, {
+    cell: 'dashboard-kpi-2',
+    type: 'KPI',
+    title: 'Hospitalization Cases',
+    subtitle: 'Total Number of Hospitalization Cases for this Year',
+    linkedValueTo: {
+        enabled: false
+    }
+    }]
